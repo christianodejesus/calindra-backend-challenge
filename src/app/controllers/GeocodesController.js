2973da4b8b475297e0b0ca8959516b2c6cf4c5d4
Christiano Marques <8249890+christianodejesus@users.noreply.github.com>
2020-09-12 01:34:18 -0300

    Getting the addresses coordinates from google maps API

--- src/app/controllers/GeocodesController.js
+++ src/app/controllers/GeocodesController.js
@@ -2,36 +2,51 @@ import { Client } from '@googlemaps/google-maps-services-js'
 import httpStatus from 'http-status'
 
 class GeocodesController {
+  static async getCoordinates (address) {
+    const gMapsClient = new Client()
+    const coordinatesResult = await gMapsClient.geocode({
+      params: {
+        address, key: process.env.GOOGLE_API_KEY
+      }
+    })
+
+    if (coordinatesResult.data.status === 'OK') {
+      return coordinatesResult.data.results[0].geometry.location
+    }
+
+    return {}
+  }
+
   async calcDistances (ctx) {
     const { addresses } = ctx.request.body
 
-    if (!addresses || !Array.isArray(addresses) || addresses.length === 0) {
+    if (!addresses || !Array.isArray(addresses) || addresses.length <= 1) {
       ctx.status = httpStatus.BAD_REQUEST
-      ctx.body = { message: 'addresses array is not provided or is an empty array' }
+      ctx.body = { message: 'The addresses list must to have at least two addresses' }
     } else {
       ctx.body = { distances: [] }
-      const client = new Client({})
 
-      addresses.forEach((address1, index, arr) => {
-        arr.forEach(async (address2, index2) => {
-          if (index2 <= index) {
-            return
-          }
+      for (const address1 of addresses) {
+        const address1Index = addresses.indexOf(address1)
 
-          const address1Coord = await client.geocode({
-            params: {
-              address: encodeURI(address1),
-              key: process.env.GOOGLE_API_KEY
-            }
-          })
+        for (let index2 = address1Index + 1; index2 <= addresses.length - 1; index2++) {
+          const address2 = addresses[index2]
 
           ctx.body.distances.push({
-            addresses: [address1, address2],
-            coordinates: [address1Coord.data, [1, 1]],
+            addresses: {
+              address1: {
+                address: address1,
+                coordinates: await GeocodesController.getCoordinates(address1)
+              },
+              address2: {
+                address: address2,
+                coordinates: await GeocodesController.getCoordinates(address2)
+              }
+            },
             distance: 0
           })
-        })
-      })
+        }
+      }
     }
   }
 }
